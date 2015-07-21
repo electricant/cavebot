d68b2f94465118ed9a78ebde861a111de4f9f51d
electricant <electricant@anche.no>
2015-07-21 17:00:42 +0200

    Fix errors within the startup script

--- start.sh
+++ start.sh
@@ -5,5 +5,11 @@
 #	mount -o bind /dev /chroot-dir/dev
 #	mount -o bind /dev/pts /chroot-dir/dev
 
+# setup locales
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export PYTHONIOENCODING=utf-8
+
 cd /srv/cavebot/
-su nobody -c "python3 CaveBot.py"
+su nobody --shell=/bin/sh -c "python3 CaveBot.py"
